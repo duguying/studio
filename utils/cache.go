7c590c9740de97472d1f60f480032af1a4004b56
Rex Lee <duguying2008@gmail.com>
2015-03-29 01:16:55 +0800

    connect redis server

--- utils/cache.go
+++ utils/cache.go
@@ -4,12 +4,25 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/cache"
 	_ "github.com/astaxie/beego/cache/memcache"
-	// "log"
+	_ "github.com/astaxie/beego/cache/redis"
+	"github.com/gogather/com/log"
 )
 
 var cc cache.Cache
 
 func InitCache() {
+	cacheConfig := beego.AppConfig.String("cache")
+
+	if "redis" == cacheConfig {
+		initRedis()
+	} else {
+		initMemcache()
+	}
+
+	log.Greenln("[cache] use", cacheConfig)
+}
+
+func initMemcache() {
 	var err error
 	cc, err = cache.NewCache("memcache", `{"conn":"`+beego.AppConfig.String("memcache_host")+`"}`)
 
@@ -17,6 +30,15 @@ func InitCache() {
 		beego.Info(err)
 	}
 
+}
+
+func initRedis() {
+	var err error
+	cc, err = cache.NewCache("redis", `{"conn":"`+beego.AppConfig.String("redis_host")+`"}`)
+
+	if err != nil {
+		beego.Info(err)
+	}
 }
 
 func SetCache(key string, value string, timeout int64) error {

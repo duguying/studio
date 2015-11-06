9a24383b00b02038657b24afed9a9a1cb170aa7c
Rex Lee <duguying2008@gmail.com>
2015-11-06 13:28:06 +0800

    重新组织grunt编译

--- static/theme/default/js/page/admin/admin.js
+++ static/theme/default/js/page/admin/admin.js
@@ -7,31 +7,31 @@ var adminService = angular.module("Admin", ['ngRoute','ng.ueditor']);
 function adminRouteConfig($routeProvider, $locationProvider){
     $routeProvider.when("/admin", {
         controller: IndexController,
-        templateUrl: "/static/ng/default.html"
+        templateUrl: "/static/theme/default/ng/default.html"
     }).when("/admin/new_article", {
         controller: NewArticleController,
-        templateUrl: "/static/ng/new_article.html"
+        templateUrl: "/static/theme/default/ng/new_article.html"
     }).when("/admin/edit_article", {
         controller: EditArticleController,
-        templateUrl: "/static/ng/edit_article.html"
+        templateUrl: "/static/theme/default/ng/edit_article.html"
     }).when("/admin/edit_article/:id", {
         controller: EditArticleController,
-        templateUrl: "/static/ng/edit_article.html"
+        templateUrl: "/static/theme/default/ng/edit_article.html"
     }).when("/admin/manage_article", {
         controller: ManageArticleController,
-        templateUrl: "/static/ng/manage_article.html"
+        templateUrl: "/static/theme/default/ng/manage_article.html"
     }).when("/admin/manage_article/:page", {
         controller: ManageArticleController,
-        templateUrl: "/static/ng/manage_article.html"
+        templateUrl: "/static/theme/default/ng/manage_article.html"
     }).when("/admin/manage_project", {
         controller: ManageProjectController,
-        templateUrl: "/static/ng/manage_project.html"
+        templateUrl: "/static/theme/default/ng/manage_project.html"
     }).when("/admin/manage_project/:page", {
         controller: ManageProjectController,
-        templateUrl: "/static/ng/manage_project.html"
+        templateUrl: "/static/theme/default/ng/manage_project.html"
     }).when("/admin/manage_oss", {
         controller: ManageOssController,
-        templateUrl: "/static/ng/manage_oss.html"
+        templateUrl: "/static/theme/default/ng/manage_oss.html"
     }).otherwise({
         redirectTo: "/admin"
     });

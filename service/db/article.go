6e83e13cba973e150bdbd3d00b43dba88da21afc
Rex Lee(李俊) <root@duguying.net>
2018-11-27 16:10:32 +0800

    fix query

--- service/db/article.go
+++ service/db/article.go
@@ -8,6 +8,7 @@ import (
 	"duguying/studio/g"
 	"duguying/studio/modules/models"
 	"github.com/gogather/json"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -69,12 +70,34 @@ func (ai *ArchInfo) String() string {
 	return string(c)
 }
 
+func (ai *ArchInfo) Parse() {
+	segs := strings.Split(ai.Date, "-")
+	if len(segs) > 1 {
+		month, _ := strconv.ParseInt(segs[1], 10, 32)
+		ai.Month = uint(month)
+	}
+	if len(segs) > 0 {
+		year, _ := strconv.ParseInt(segs[0], 10, 32)
+		ai.Year = uint(year)
+	}
+}
+
+func sortAndParse(arch []*ArchInfo) []*ArchInfo {
+	sortArch := []*ArchInfo{}
+	for _, item := range arch {
+		item.Parse()
+		sortArch = append([]*ArchInfo{item}, sortArch...)
+	}
+	return sortArch
+}
+
 func MonthArch() (archInfos []*ArchInfo, err error) {
 	archInfos = []*ArchInfo{}
-	errs := g.Db.Table("articles").Select("DATE_FORMAT(created_at,'%Y-%m') as date,count(*) as number ,year(created_at) as year, month(created_at) as month").Where("status=?", 1).Group("date").Order("year desc, month desc").Find(&archInfos).GetErrors()
+	errs := g.Db.Table("articles").Select("DATE_FORMAT(created_at,'%Y-%m') as date,count(*) as number").Where("status=?", 1).Group("date").Find(&archInfos).GetErrors()
 	if len(errs) > 0 && errs[0] != nil {
 		return nil, errs[0]
 	}
+	archInfos = sortAndParse(archInfos)
 	for _, arch := range archInfos {
 		arch.Date = strings.Replace(arch.Date, "-", "年", -1) + "月"
 	}

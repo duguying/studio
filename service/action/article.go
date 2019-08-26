2ad686ab8dd6e8a2ec45f0aeb4d3916c18549c8a
Rex Lee(李俊) <root@duguying.net>
2019-08-26 15:45:55 +0800

    rollback

--- service/action/article.go
+++ service/action/article.go
@@ -9,7 +9,6 @@ import (
 	"duguying/studio/modules/dbmodels"
 	"duguying/studio/utils"
 	"fmt"
-	"github.com/Unknwon/com"
 	"github.com/gin-gonic/gin"
 	"github.com/gogather/json"
 	"log"
@@ -359,7 +358,7 @@ func SiteMap(c *gin.Context) {
 
 	// articles
 	for _, item := range list {
-		sitemap = append(sitemap, fmt.Sprintf("/article/%s", com.UrlEncode(item.Uri)))
+		sitemap = append(sitemap, fmt.Sprintf("/article/%s", item.Uri))
 	}
 
 	// list pages

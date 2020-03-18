e969222601489f72e40efacb3567779738d0d3b2
Rainesli(李俊) <rainesli@tencent.com>
2020-03-18 14:43:44 +0800

    update

--- utils/utils.go
+++ utils/utils.go
@@ -5,11 +5,9 @@
 package utils
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha1"
 	"fmt"
-	"github.com/mozillazg/go-pinyin"
 	"github.com/satori/go.uuid"
 	"net/http"
 	"os"
@@ -43,27 +41,6 @@ func GetFileContentType(out *os.File) (string, error) {
 	contentType := http.DetectContentType(buffer)
 
 	return contentType, nil
-}
-
-func TitleToUri(title string) (uri string) {
-	var buffer bytes.Buffer
-	for _, c := range title {
-		char := fmt.Sprintf("%c", c)
-		pys := pinyin.LazyConvert(char, nil)
-		pystr := strings.Join(pys, "")
-		if pystr != "" {
-			buffer.WriteString(pystr + "-")
-		} else {
-			buffer.WriteString(char)
-		}
-	}
-	uri = buffer.String()
-	uri = strings.Replace(uri, "\n", "", -1)
-	uri = strings.Replace(uri, "\r", "", -1)
-	uri = strings.Replace(uri, "\t", " ", -1)
-	uri = strings.Replace(uri, " ", "-", -1)
-	uri = strings.TrimSuffix(uri, "-")
-	return uri
 }
 
 func StrContain(keyword string, vendor []string) bool {
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -6,12 +6,6 @@ import (
 	"testing"
 )
 
-func TestTitleToUri(t *testing.T) {
-	hans := "中国人is chinese"
-	py := TitleToUri(hans)
-	fmt.Println(py)
-}
-
 func TestGenUUID(t *testing.T) {
 	test := "/art/1+1=2"
 	fmt.Println(com.UrlEncode(test))

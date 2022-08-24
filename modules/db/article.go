620fa58be26c493bb1faeb6a6f5027e3de40a453
Rex Lee(李俊) <root@duguying.net>
2022-08-24 12:09:34 +0800

    支持更新tags

--- modules/db/article.go
+++ modules/db/article.go
@@ -361,6 +361,9 @@ func UpdateArticle(tx *gorm.DB, id uint, article *models.Article) (err error) {
 	if article.URI != "" {
 		fields["uri"] = article.URI
 	}
+	if len(article.Keywords) > 0 {
+		fields["tags"] = strings.Join(article.Keywords, ",")
+	}
 	err = tx.Model(dbmodels.Article{}).Where("id=?", id).Updates(fields).Error
 	if err != nil {
 		return err
